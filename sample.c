4f37321e85a11c0be98b943cec42ee03bea2fb8f
reotak <reotak@gmail.com>
2015-08-02 22:36:28 +0900

    subver

--- sample.c
+++ sample.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-#define VER 2.2
+#define VER 2
+#define SUB_VER 2
+
 
 char *comment() {
     return "test3 develop started!!!";
 }
 
 int main() {
-    printf("%lf : %s\n", VER, comment());
+    printf("%d.%d : %s\n", VER, SUB_VER, comment());
 }
